e958453f6064a97429f7c1d9e63de012d57e3895
Andrew Shugarts <rue@ruethedev.com>
2023-03-20 00:53:20 -0400

    Update ssh perms script

--- run_fix_ssh_perms.sh
+++ run_fix_ssh_perms.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-chmod 600 ~/.ssh/github
+#!/bin/sh
+
+FILE="$HOME/.ssh/github"
+if [ -f "$FILE" ]; then
+    if [ "$(stat -c %a "$FILE")" != "600" ] ; then
+        chmod 600 "$FILE"
+    fi
+fi
+
+
