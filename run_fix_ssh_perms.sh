ed0c97f2a1cbd53d0dbc0b3d9c07b397073bc47c
Andrew Shugarts <rue@ruethelemur.fios-router.home>
2023-11-20 22:49:39 -0500

    Adjust SSH keys

--- run_fix_ssh_perms.sh
+++ run_fix_ssh_perms.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-FILE="$HOME/.ssh/github"
-if [ -f "$FILE" ]; then
-    if [ "$(stat -c %a "$FILE")" != "600" ] ; then
-        chmod 600 "$FILE"
+SSH_FILES=("$HOME/.ssh/github" "$HOME/.ssh/digital_ocean")
+for FILE in ${SSH_FILES[@]}; do
+    if [ -f "$FILE" ]; then
+        if [ "$(stat -c %a "$FILE")" != "600" ] ; then
+            chmod 600 "$FILE"
+        fi
     fi
-fi
+done
 
 
